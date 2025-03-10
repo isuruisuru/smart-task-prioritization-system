f0f7ae6a1c46627a760fbafc6a3457454c430092
Chamod Isuru <92633908+isuruisuru@users.noreply.github.com>
2025-03-10 14:43:16 +0530

    delete task imp

--- backend/src/routes/taskRoutes.js
+++ backend/src/routes/taskRoutes.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { createTask, getTask, getTasks, updateTask } from "../controllers/task/taskController.js";
+import { createTask, deleteTask, getTask, getTasks, updateTask } from "../controllers/task/taskController.js";
 import { protect } from "../middleware/authMiddleware.js";
 
 const router  = express.Router();
@@ -8,5 +8,6 @@ router.post("/task/create", protect, createTask);
 router.get("/tasks", protect, getTasks);
 router.get("/task/:id", protect, getTask);
 router.patch("/task/:id", protect, updateTask);
+router.delete("/task/:id", protect, deleteTask);
 
 export default router;
