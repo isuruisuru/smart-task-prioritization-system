56397e5a19530bab93b09cab3f377a8610163113
Chamod Isuru <92633908+isuruisuru@users.noreply.github.com>
2025-03-28 13:48:38 +0530

    imp menubar, main layout, sidebar provider, chart

--- client/app/layout.tsx
+++ client/app/layout.tsx
@@ -1,8 +1,14 @@
 import { Toaster } from "react-hot-toast";
 import UserProvider from "@/providers/UserProvider";
 import type { Metadata } from "next";
-import { Geist, Geist_Mono, Inter } from "next/font/google";
+import { Inter } from "next/font/google";
 import "./globals.css";
+import MiniSidebar from "./Components/MiniSidebar/MiniSidebar";
+import Header from "./Components/Header/Header";
+import SidebarProvider from "@/providers/SidebarProvider";
+import MainContentLayout from "@/providers/MainContentLayout";
+import MainLayout from "@/providers/MainLayout";
+
 
 const inter = Inter({
   subsets: ["latin"]
@@ -20,11 +26,23 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css" integrity="sha512-Evv84Mr4kqVGRNSgIGL/F/aIDqQb7xQ2vcrdIwxfjThSH8CSR7PBEakCr51Ck+w+/U6swU2Im1vVX0SVk9ABhg==" crossOrigin="anonymous" referrerPolicy="no-referrer" />
+      </head>
       <body
         className={inter.className}
       >
         <UserProvider>
-          {children}
+          <div className="h-full flex overflow-hidden">
+            <MiniSidebar /> 
+            <div className="flex-1 flex flex-col">
+              <Header />
+              <MainContentLayout>
+                <MainLayout>{children}</MainLayout>
+                <SidebarProvider />
+              </MainContentLayout>
+            </div>
+          </div>
         </UserProvider>
         <Toaster position="top-center" />
       </body>
---
+++ client/providers/SidebarProvider.tsx
@@ -0,0 +1,16 @@
+"use client";
+import { useUserContext } from '@/context/userContext';
+import Sidebar from '@/app/Components/Sidebar/Sidebar'
+import React from 'react'
+
+
+function SidebarProvider() {
+  const userId = useUserContext().user._id;
+  return (
+    <>
+    {userId && <Sidebar />}
+    </>
+  )
+}
+
+export default SidebarProvider
