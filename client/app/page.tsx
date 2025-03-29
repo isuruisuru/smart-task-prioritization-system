039eb3464dedada493556494d14cf9c6c267b18c
Chamod Isuru <92633908+isuruisuru@users.noreply.github.com>
2025-03-29 20:59:44 +0530

    frontend crud imp task imp nd other feat

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -1,10 +1,19 @@
 "use client";
 import useRedirect from "@/hooks/useUserRedirect";
+import { useTasks } from "@/context/taskContext";
+import Filters from "./Components/Filters/Filters";
 
 export default function Home() {
   useRedirect("/login");
 
+  const { tasks } = useTasks();
+
   return (
-    <main></main>
+    <main className="m-6 h-full">
+      <div className="flex justify-between">
+        <h1 className="text-xl font-bold">All Tasks</h1>
+        <Filters />
+      </div>
+    </main>
   );
 }
